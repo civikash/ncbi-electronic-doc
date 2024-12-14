11bab40c19b41340417eb34698b38dc5af675fd3
civikash <kashurkin.v@yandex.ru>
2024-12-14 19:43:40 +0300

    Добавлена установка netcat в контейнер

--- ncbi_sed_standalone/db.prod.sh
+++ ncbi_sed_standalone/db.prod.sh
@@ -2,16 +2,14 @@
 
 if [ "$PG_DATABASE" = "$PG_DATABASE" ]
 then
-    echo "Ожидание..."
-    echo $PG_DATABASE
-    echo $POSTGRES_HOST
-    echo $POSTGRES_PORT
+    echo "Ожидание PostgreSQL на $POSTGRES_HOST:$POSTGRES_PORT..."
+    echo "База данных: $PG_DATABASE"
 
     while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
-      sleep 0.1
+        sleep 0.1
     done
 
-    echo "PostgreSQL запущен"
+    echo "PostgreSQL запущен и доступен"
 fi
 
 exec "$@"
