98848657510fd628054c970638bc754c37088bd1
civikash <kashurkin.v@yandex.ru>
2024-12-14 17:53:23 +0300

    Изменен скрипт db.prod.sh

--- ncbi_sed_standalone/db.prod.sh
+++ ncbi_sed_standalone/db.prod.sh
@@ -3,6 +3,9 @@
 if [ "$PG_DATABASE" = "$PG_DATABASE" ]
 then
     echo "Ожидание..."
+    echo $PG_DATABASE
+    echo $POSTGRES_HOST
+    echo $POSTGRES_PORT
 
     while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
       sleep 0.1
